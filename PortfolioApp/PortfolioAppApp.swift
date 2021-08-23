f3bfaaf56a00821b5260b3557863c65b91e4777c
rimas12 <rizky_mashudy@yahoo.co.id>
2021-08-23 22:38:36 +0700

    Initial Commit

---
+++ PortfolioApp/PortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PortfolioAppApp.swift
+//  PortfolioApp
+//
+//  Created by Rizky Mashudi on 23/08/21.
+//
+
+import SwiftUI
+
+@main
+struct PortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
